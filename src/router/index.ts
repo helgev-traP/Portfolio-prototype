6c25ab7a6c98e2f29f068ec9cb340e47282d6e1b
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-09-25 01:08:31 +0900

    Cleared vue default. Project start from here.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TopPage from '../pages/topPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'topPgae',
+      component: TopPage
+    },
   ]
 })
 
