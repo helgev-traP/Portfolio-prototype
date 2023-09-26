379104146ebffbfbc3a75baf956f5014c273f862
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-09-27 03:05:14 +0900

    log

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TopPage from './pages/topPage.vue'
+import PageHeader from './UI/PageHeader.vue'
 </script>
 
 <template>
   <div :class="$style.container">
-    <RouterView />
+    <header :class="$style.header">
+      <page-header />
+    </header>
+    <top-page />
   </div>
 </template>
 
-<style lang="scss" module scoped>
+<style lang="scss" module>
 .container{
+  display: grid;
+  grid-template-rows: min-content 1fr;
+  background-color: $color-background;
 
+}
+.header{
+  grid-row: 1;
 }
 </style>
---
+++ src/UI/PageHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="$style.header">へっだーとかの中身</div>
+</template>
+
+<style lang="scss" module>
+.header {
+  display: grid;
+  padding: 1rem;
+  border-bottom: 2px solid;
+  border-bottom-color: black;
+}
+</style>
